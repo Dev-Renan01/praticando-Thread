dfa5e71cfcf05b34fa384e922cc981f364012983
Thiago Renan <thiagorenan1108@gmail.com>
2025-06-16 15:24:57 -0300

    Introdução a Thread

--- Threads/src/Main.java
+++ Threads/src/Main.java
@@ -1,7 +1,49 @@
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
-        System.out.printf("Hello and welcome!");
+        new Thread(){
+            public void run() {/*Executa o que queremos*/
+                /*Código da rotina*/
+                for (int i = 0; i < 10; i++) {
+
+                    System.out.println("Executando uma rotina, EX: envio de E-mail!");
+
+                    try {
+                        Thread.sleep(1000);
+                        /*Quero executar esse envio com um tempo de parada / tempo determinado*/
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();/* O start liga a thread que fica processando paralelamente por trás*/
+
+
+        //**************** DIVISÂO DAS TREADS
+
+
+        new Thread(){
+            public void run() {/*Executa o que queremos*/
+                /*Código da rotina*/
+                for (int i = 0; i < 10; i++) {
+
+                       System.out.println("Executando uma rotina, EX: Envio de Nota fiscal!");
+
+                    try {
+                        Thread.sleep(1000);
+                        /*Quero executar esse envio com um tempo de parada / tempo determinado*/
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();/* O start liga a thread que fica processando paralelamente por trás*/
+
+        System.out.println("Cregou ao fim do código de teste !");
+
+
+
+
     }
 }
