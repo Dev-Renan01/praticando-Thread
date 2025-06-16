65ff30c695c0ab568c2e333259eafb86cd41bedd
Thiago Renan <thiagorenan1108@gmail.com>
2025-06-16 15:47:54 -0300

    Thread em objeto e o Runnable

--- Threads/src/Main.java
+++ Threads/src/Main.java
@@ -1,49 +1,52 @@
 
 public class Main {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
-        new Thread(){
-            public void run() {/*Executa o que queremos*/
-                /*Código da rotina*/
-                for (int i = 0; i < 10; i++) {
-
-                    System.out.println("Executando uma rotina, EX: envio de E-mail!");
-
-                    try {
-                        Thread.sleep(1000);
-                        /*Quero executar esse envio com um tempo de parada / tempo determinado*/
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }.start();/* O start liga a thread que fica processando paralelamente por trás*/
-
+        Thread threadNotaFiscal = new Thread(thread1);
+        threadNotaFiscal.start();
 
         //**************** DIVISÂO DAS TREADS
 
-
-        new Thread(){
-            public void run() {/*Executa o que queremos*/
-                /*Código da rotina*/
-                for (int i = 0; i < 10; i++) {
-
-                       System.out.println("Executando uma rotina, EX: Envio de Nota fiscal!");
-
-                    try {
-                        Thread.sleep(1000);
-                        /*Quero executar esse envio com um tempo de parada / tempo determinado*/
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }.start();/* O start liga a thread que fica processando paralelamente por trás*/
+       Thread threadEmail = new Thread(thread2);
+       threadEmail.start();
 
         System.out.println("Cregou ao fim do código de teste !");
-
-
-
-
     }
+
+
+    private static Runnable thread1 = new Runnable() {
+
+        @Override
+        public void run() {
+            /*Código da rotina*/
+            for (int i = 0; i < 10; i++) {
+
+                System.out.println("Executando uma rotina, EX: Envio de Nota fiscal!");
+
+                try {
+                    /*Quero executar esse envio com um tempo de parada / tempo determinado*/
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    private static Runnable thread2 = new Runnable() {
+
+        @Override
+        public void run() {
+                /*Código da rotina*/
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Executando uma rotina, EX: envio de E-mail!");
+                try {
+                    Thread.sleep(1000);
+                    /*Quero executar esse envio com um tempo de parada / tempo determinado*/
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
 }
